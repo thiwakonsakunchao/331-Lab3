bfe56b172db568786809d104dabcb984285cc603
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-08-06 23:10:50 +0700

    1.10

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <RouterLink :to="{name: 'event-detail',params:{id: event?.id}}">
+  <RouterLink class="event-link" :to="{name: 'event-detail',params:{id: event?.id}}">
     <div class="event-class">
     <div class="event-card">
       <span>@{{ event?.time }} on {{ event?.date }}</span>
@@ -32,5 +32,10 @@ const props = defineProps({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link{
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
