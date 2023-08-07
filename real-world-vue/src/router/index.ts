32cb2364cfeb22935240222abba5e7a7499af0ff
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-08-07 20:46:08 +0700

    4.1

--- real-world-vue/src/router/index.ts
+++ real-world-vue/src/router/index.ts
@@ -7,7 +7,7 @@ import EventDetailView from '../views/event/EventDetailView.vue'
 import EventEditView from '@/views/event/EventEditView.vue'
 import EventRegisterView from '@/views/event/EventRegisterView.vue'
 import EventLayoutView from '@/views/event/EventLayoutView.vue'
-
+import NotFoundView from "../views/NotFoundView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -64,6 +64,18 @@ const router = createRouter({
         }
       ]
       
+    },{
+        path: '/404/:resource',
+        name: '404-resource',
+        component: NotFoundView,
+        props:true
+    },
+    {
+
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
+
     }
     
     
--- real-world-vue/src/views/event/EventLayoutView.vue
+++ real-world-vue/src/views/event/EventLayoutView.vue
@@ -2,18 +2,20 @@
 import  { ref } from 'vue'
 import  { type EventItem } from '@/type';
 import EventService from '@/services/EventService';
+import { useRouter } from 'vue-router';
 const event = ref<EventItem | null>(null)
 
 const props = defineProps({
     id: String
 })
-
+const router = useRouter()
 EventService.getEventById(Number(props.id))
     .then((response) => {
      event.value = response.data
 })
     .catch(error => {
     console.log(error)
+    router.push({name: '404-resource', params:{ resource: 'event'}})
 })
 
 </script>
